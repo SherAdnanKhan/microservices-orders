930bb861407dddabbe2bf639f1b1b9716b59d196
Salwa M <codernist.s@gmail.com>
2020-05-06 18:36:09 +0500

    added design of lobby screen and also refactered some code in signup

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -49,3 +49,28 @@ $(document).on('click', '.passwordEye', async function () {
 // async function getCurrentStepNo() {
 //   return $('.registerationScreen div[active="true"]').attr('step');
 // }
+
+$(document).on('click', '.feelIcon', async function () {
+  $('.colorChangerScreen').show();
+});
+
+$(document).on('click', '.colorChangerScreen img[color]', async function () {
+  const getColor = $(this).attr('color');
+  if (getColor != undefined) {
+    $('.frameReady')
+      .removeClass('gold gray red purple dodgerblue orange limegreen')
+      .addClass(getColor);
+  }
+  $('.colorChangerScreen').hide();
+});
+
+$(document).on('click', '.menuBlock', function () {
+  const checkNav = $('nav').css('display');
+  if (checkNav == 'none') {
+    $('nav').show();
+    $(this).find('.fas').removeClass('fa-arrow-left fa-ellipsis-v').addClass('fa-arrow-left');
+  } else {
+    $('nav').hide();
+    $(this).find('.fas').removeClass('fa-arrow-left fa-ellipsis-v').addClass('fa-ellipsis-v');
+  }
+});
