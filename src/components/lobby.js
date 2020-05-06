51274ead6c1ecfed1f778b8fb704d67531f11463
Salwa M <codernist.s@gmail.com>
2020-05-06 14:37:35 +0500

    added and implemented logout button in lobby page

--- src/components/lobby.js
+++ src/components/lobby.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { logout } from '../actions/authActions';
 
 const Lobby = () => {
   return (
     <div className="lobby">
       <div className="lobbyText">
         Lobby
+      </div>
+      <div className="logout">
+        <button className="btn clickable" onClick={() => logout()}> Logout </button>
       </div>
     </div>
   );
