87b778a57ee7004a08d8b92fdfa454aece3b6a23
Salwa M <codernist.s@gmail.com>
2020-05-08 11:50:12 +0500

    added ind or after signup screen page

---
+++ src/components/ind.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+const Ind = () => {
+  return (
+    <div className="selectMood">
+      <div className="cubecenter">
+        <div className="procufull">
+          <div className="scenefull">
+            <div className="cubefull">
+              <div className="cube-facefull  bordermenufeel cube-face-frontfull" style={{ borderColor: 'red' }}>
+                <img alt="" src="./assets/images/dummyData/03.png" height="100%" />
+              </div>
+              <div className="cube-facefull  bordermenufeel cube-face-backfull" style={{ borderColor: 'red' }}>
+                <img alt="" src="./assets/images/dummyData/03.png" height="100%" />
+              </div>
+              <div className="cube-facefull  bordermenufeel cube-face-leftfull" style={{ borderColor: 'red' }}>
+                <img alt="" src="./assets/images/dummyData/03.png" height="100%" />
+              </div>
+              <div className="cube-facefull bordermenufeel cube-face-rightfull" style={{ borderColor: 'red' }}>
+                <img alt="" src="./assets/images/dummyData/03.png" height="100%" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="welcomeText">
+        Hi Welcome<br />
+      Second line
+      </div>
+      <div className="happy">
+        <img alt="" src="./assets/images/expressions/iconyellow.png" color="gold" />
+        Happy
+      </div>
+      <div className="confused">
+        <img alt="" src="./assets/images/expressions/icongray.png" color="gray" />
+        Confused
+      </div>
+      <div className="excited">
+        <img alt="" src="./assets/images/expressions/iconorange.png" color="orange" />
+        Excited
+      </div>
+      <div className="serene">
+        <img alt="" src="./assets/images/expressions/icongreen.png" color="limegreen" />
+        Serene
+      </div>
+      <div className="angry">
+        <img alt="" src="./assets/images/expressions/iconred.png" color="red" />
+        Angry
+      </div>
+      <div className="sad">
+        <img alt="" src="./assets/images/expressions/iconblue.png" color="dodgerblue" />
+        Sad
+      </div>
+      <div className="inspired">
+        <img alt="" src="./assets/images/expressions/iconpurple.png" color="purple" />
+      Sad
+      </div>
+    </div>
+  );
+};
+
+export default Ind;
