d0cc946327881394c188d425ece597e9e104a45b
Salwa M <codernist.s@gmail.com>
2020-05-11 15:54:48 +0500

    save feel icon in localstorage

--- src/components/dashboard/Main.js
+++ src/components/dashboard/Main.js
@@ -1,11 +1,13 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useEffect, useState } from 'react';
 import Header from './layout/header';
 import LeftBorder from './layout/leftBorder';
 import RightBorder from './layout/rightBorder';
 import Footer from './layout/footer';
 import { useLocation } from 'react-router-dom';
 
+
 const Main = () => {
+  const [color, setColor] = useState('red');
   const location = useLocation();
   const split = location.pathname.split('/');
 
@@ -22,9 +24,19 @@ const Main = () => {
     }
   });
 
+  useEffect(() => {
+    if (localStorage.color)
+      setColor(JSON.parse(localStorage.getItem('color')));
+  }, []);
+
+  const handleColorChange = color => {
+    setColor(color);
+    localStorage.setItem('color', JSON.stringify(color));
+  };
+
   return (
-    <div className="frameReady red">
-      <Header />
+    <div className={`frameReady ${color}`}>
+      <Header onColorChange={handleColorChange} />
       <LeftBorder />
       <RightBorder />
       <Suspense fallback={<div></div>}>
--- src/components/dashboard/layout/header.js
+++ src/components/dashboard/layout/header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import NavBar from './navBar';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({ onColorChange }) => {
   return (
     <>
       <div className="top">
@@ -26,31 +26,31 @@ const Header = () => {
         <div className="centerCenter">
           <div className="seventy">
             <div className="l">
-              <img alt="" src="/assets/images/expressions/iconyellow.png" color="gold" />
+              <img alt="" src="/assets/images/expressions/iconyellow.png" color="gold" onClick={e => onColorChange(e.currentTarget.attributes.color.value)} />
             </div>
             <div className="r">
-              <img alt="" src="/assets/images/expressions/icongray.png" color="gray" />
+              <img alt="" src="/assets/images/expressions/icongray.png" color="gray" onClick={e => onColorChange(e.currentTarget.attributes.color.value)} />
             </div>
           </div>
           <div className="ninety">
             <div className="l">
-              <img alt="" src="/assets/images/expressions/iconorange.png" color="orange" />
+              <img alt="" src="/assets/images/expressions/iconorange.png" color="orange" onClick={e => onColorChange(e.currentTarget.attributes.color.value)} />
             </div>
             <div className="r">
-              <img alt="" src="/assets/images/expressions/icongreen.png" color="limegreen" />
+              <img alt="" src="/assets/images/expressions/icongreen.png" color="limegreen" onClick={e => onColorChange(e.currentTarget.attributes.color.value)} />
             </div>
           </div>
           <div className="seventy">
             <div className="l">
-              <img alt="" src="/assets/images/expressions/iconred.png" color="red" />
+              <img alt="" src="/assets/images/expressions/iconred.png" color="red" onClick={e => onColorChange(e.currentTarget.attributes.color.value)} />
             </div>
             <div className="r">
-              <img alt="" src="/assets/images/expressions/iconpurple.png" color="purple" />
+              <img alt="" src="/assets/images/expressions/iconpurple.png" color="purple" onClick={e => onColorChange(e.currentTarget.attributes.color.value)} />
             </div>
           </div>
           <div className="single">
             <div className="c">
-              <img alt="" src="/assets/images/expressions/iconblue.png" color="dodgerblue" />
+              <img alt="" src="/assets/images/expressions/iconblue.png" color="dodgerblue" onClick={e => onColorChange(e.currentTarget.attributes.color.value)} />
             </div>
           </div>
         </div>
