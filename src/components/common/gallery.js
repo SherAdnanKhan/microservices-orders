f030afc23123853e5729933836575e7be27be940
Salwa M <codernist.s@gmail.com>
2020-05-15 13:32:59 +0500

    fixed my studio page and consumed galleries api

---
+++ src/components/common/gallery.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Gallery = ({ galleries, edit, onGalleryChange }) => {
+  return (
+    <>
+      {edit && galleries &&
+        <div className="screen">
+          <div className="scr-inner">
+            {galleries.map((gallery, index) => (
+              <div key={index} className={`item-box item-box-${index + 1}`} onClick={() => onGalleryChange(gallery)}>
+                <img src="/assets/images/galleryicon.png" alt="" />
+              </div>
+            ))}
+          </div>
+        </div>
+      }
+      {!edit && galleries &&
+        <div className="edit-screen">
+          <div className="scr-inner">
+            {galleries.map((gallery, index) => (
+              <div key={index} className={`item-box item-box-${index + 1}`}>
+                <div className="editTool Edit">
+                  <img src="/assets/images/paintbrush.png" alt="" />
+                </div>
+                <img src="/assets/images/galleryicon.png" alt="" />
+              </div>
+            ))}
+          </div>
+        </div>
+      }
+    </>
+  );
+};
+
+export default Gallery;
