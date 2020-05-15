6ff6c45f95cdeebc455cc5397e1f8800b3a6677b
Salwa M <codernist.s@gmail.com>
2020-05-15 20:35:06 +0100

    sidebar

--- src/components/dashboard/layout/rightBorder.js
+++ src/components/dashboard/layout/rightBorder.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const RightBorder = () => {
   return (
-    <div className="right" />
+    <div className="right">
+      <img alt="" src="/assets/images/strqicon.png" />
+      <img alt="" src="/assets/images/mzflash.png" />
+    </div>
   );
 };
 
