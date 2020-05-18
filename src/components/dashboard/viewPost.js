19263b54f88cefb34c7f758814a731970460a34f
Salwa M <codernist.s@gmail.com>
2020-05-18 12:15:18 +0500

    linkage of pages

--- src/components/dashboard/viewPost.js
+++ src/components/dashboard/viewPost.js
@@ -27,32 +27,31 @@ const ViewPost = () => {
      dispatch(makeStoke(ID,id));
    }
 
- 
 return (
-  <div className="stokes-page">
+  <div className="post-page">
      {loading && <Spinner />}
      { strokes &&
-        <div className="stoke-head"> 
+        <div className="post-head"> 
           <p>Akif</p>
           {/* <StrokesCube avatars={strokes && strokes.post && strokes.post.image} /> */}
           {/* <Avatar avatars={strokes && strokes.post && strokes.post.image} /> */}
           { strokes && strokes.post && strokes.post.art && strokes.post.art.parent && <p>{strokes.post.art.parent.name}/{strokes.post.art.name}</p>}
         </div>
      }
-      <div className="stoke-body">
+      <div className="post-body">
        { strokes && strokes.post && strokes.post.image && strokes.post.image.path && <img src={strokes.post.image.path} alt="" stye={{width:"100%", heigth:"100%"}} />}
       </div>
-      <div className="stoke-footer">
+      <div className="post-footer">
         { strokes && strokes.post && strokes.post.title && <h3>{strokes.post.title}</h3> } 
         <div className="stoke-icon-div">
           { strokes && strokes.has_stroke ? ( 
-          <div className="stoke-icon-set">
-            <img className="stoke-color-icon" src="/assets/images/strokeiconem.png" alt="" onClick={(e) => handleUnStoke(e,strokes && strokes.post && strokes.post.id)}/>
+          <div className="post-icon-set">
+            <img className="post-color-icon" src="/assets/images/strokeiconem.png" alt="" onClick={(e) => handleUnStoke(e,strokes && strokes.post && strokes.post.id)}/>
             <h3>strokes 1</h3>
           </div>  
           ) : (
-          <div className="stoke-icon-set">
-            <img className="stoke-non-color-icon" src="/assets/images/strokeiconem.png" alt="" onClick={(e) => handleStoke(e,strokes && strokes.post && strokes.post.id)} />
+          <div className="post-icon-set">
+            <img className="post-non-color-icon" src="/assets/images/strokeiconem.png" alt="" onClick={(e) => handleStoke(e,strokes && strokes.post && strokes.post.id)} />
             <h3>strokes 0</h3>
           </div>)
           } 
