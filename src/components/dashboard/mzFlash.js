8771d09d641626e61811ca32e950f68f921f7602
Salwa M <codernist.s@gmail.com>
2020-05-21 11:15:02 +0500

    fixed mz flash faving button issue

--- src/components/dashboard/mzFlash.js
+++ src/components/dashboard/mzFlash.js
@@ -1,70 +1,64 @@
-import React, { useEffect, Fragment } from "react"
+import React, { useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import ProfileCube from '../common/profileCube';
-import { makeUserFav, UserUnFav } from "../../actions/userActions";
+import { favUser, unfavUser } from "../../actions/userActions";
 import { useRouteMatch } from 'react-router-dom';
 import { getUserStudio } from "../../actions/studioActions";
-import Spinner from '../common/spinner';
 
 const MzFlash = () => {
   const dispatch = useDispatch();
   const {
     studio: { userStudio },
-    loading: { loading }
   } = useSelector(state => state);
 
   const { params: { slug } } = useRouteMatch();
 
-  function handleFave(e,id){
+  function handleFave(e, id) {
     e.preventDefault();
-    dispatch(makeUserFav(id,slug))
+    dispatch(favUser(id))
   }
 
-  function handleUnFave(e,id){
+  function handleUnFave(e, id) {
     e.preventDefault();
-    dispatch(UserUnFav(id,slug))
+    dispatch(unfavUser(id))
   }
 
   useEffect(() => {
     dispatch(getUserStudio(slug));
-}, [dispatch,slug]);
+  }, [dispatch, slug]);
 
   return (
     <div className="mz-flash-page">
-       {loading && <Spinner />}
-       {!loading &&
-       <Fragment>
-         <div className="mz-flash-head">
-         {  userStudio &&
+      <>
+        <div className="mz-flash-head">
+          {userStudio &&
             <div className="fav-amount">
-               <p>Amount Faves {userStudio.fav_by_count}</p>
-               <p>Amount Faving {userStudio.favs_count}</p>
+              <p>Amount Faves {userStudio.fav_by_count}</p>
+              <p>Amount Faving {userStudio.favs_count}</p>
             </div>
-         }
-       <div className="fav-cub">
-         { userStudio &&
-             <div className="procu">
-               <ProfileCube avatars={userStudio?.user?.avatars} />
-             </div>
           }
-       </div>
-       <div className="fav-btn-div">
-         {
-           userStudio && userStudio.has_faved &&
-             <button className="fav-btn" onClick={(e) => handleUnFave(e,userStudio.user.id)}>FAVING</button>
-         }
-         {
-           userStudio && !userStudio.has_faved &&
-            <button className="fav-btn" onClick={(e) => handleFave(e,userStudio.user.id)}>FAVE</button>
-         }
-       </div>
-     </div>
-        
+          <div className="fav-cub">
+            {userStudio &&
+              <div className="procu">
+                <ProfileCube avatars={userStudio?.user?.avatars} />
+              </div>
+            }
+          </div>
+          <div className="fav-btn-div">
+            {userStudio && userStudio.has_faved &&
+              <button className="fav-btn clickable" onClick={(e) => handleUnFave(e, userStudio.user.id)}>FAVING</button>
+            }
+            {userStudio && !userStudio.has_faved &&
+              <button className="fav-btn clickable" onClick={(e) => handleFave(e, userStudio.user.id)}>FAVE</button>
+            }
+          </div>
+        </div>
+
         <div className="fav-bar">
           <h3>MZ FLASH</h3>
         </div>
-      </Fragment>
-    }
+      </>
+
     </div>
   )
 
