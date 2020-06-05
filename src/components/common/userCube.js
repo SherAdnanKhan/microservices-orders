d2e750ba56da5f9fd8a7dcd8110d931839ef0e80
Salwa M <codernist.s@gmail.com>
2020-06-05 19:11:23 +0500

    fixed some css design issues and implemented document upload

--- src/components/common/userCube.js
+++ src/components/common/userCube.js
@@ -2,76 +2,76 @@ import React from 'react';
 
 const UserCube = ({ user }) => {
   return (
-    <div className="cubescroll">
+    <div className={`cubescroll ${user.feel_color}`}>
       <div className="procu_">
         <div className="scene">
-            {user.avatars && user.avatars.length > 0 &&
-              <div className="cube">
-                {user.avatars.length === 1 &&
-                  <>
-                    <div className="cube-face  cube-face-front">
-                      <img alt="" src={user.avatars[0].path} height="100%" />
-                    </div>
-                    <div className="cube-face  cube-face-back" style={{ borderColor: 'gold' }}>
-                      <img alt="" src={user.avatars[0].path} height="100%" />
-                    </div>
-                    <div className="cube-face  cube-face-left" style={{ borderColor: 'gold' }}>
-                      <img alt="" src={user.avatars[0].path} height="100%" />
-                    </div>
-                    <div className="cube-face  cube-face-right" style={{ borderColor: 'gold' }}>
-                      <img alt="" src={user.avatars[0].path} height="100%" />
-                    </div>
-                  </>
-                }
-                {user.avatars.length === 2 &&
-                  <>
-                    <div className="cube-face  cube-face-front">
-                      <img alt="" src={user.avatars[0].path} height="100%" />
-                    </div>
-                    <div className="cube-face  cube-face-back" style={{ borderColor: 'gold' }}>
-                      <img alt="" src={user.avatars[1].path} height="100%" />
-                    </div>
-                    <div className="cube-face  cube-face-left" style={{ borderColor: 'gold' }}>
-                      <img alt="" src={user.avatars[0].path} height="100%" />
-                    </div>
-                    <div className="cube-face  cube-face-right" style={{ borderColor: 'gold' }}>
-                      <img alt="" src={user.avatars[1].path} height="100%" />
-                    </div>
-                  </>
-                }
-                {user.avatars.length === 3 &&
-                  <>
-                    <div className="cube-face  cube-face-front">
-                      <img alt="" src={user.avatars[0].path} height="100%" />
-                    </div>
-                    <div className="cube-face  cube-face-back" style={{ borderColor: 'gold' }}>
-                      <img alt="" src={user.avatars[1].path} height="100%" />
-                    </div>
-                    <div className="cube-face  cube-face-left" style={{ borderColor: 'gold' }}>
-                      <img alt="" src={user.avatars[2].path} height="100%" />
-                    </div>
-                    <div className="cube-face  cube-face-right" style={{ borderColor: 'gold' }}>
-                      <img alt="" src={user.avatars[0].path} height="100%" />
-                    </div>
-                  </>
-                }
-                {user.avatars.length >= 4 &&
-                  <>
-                    <div className="cube-face  cube-face-front">
-                      <img alt="" src={user.avatars[0].path} height="100%" />
-                    </div>
-                    <div className="cube-face  cube-face-back" style={{ borderColor: 'gold' }}>
-                      <img alt="" src={user.avatars[1].path} height="100%" />
-                    </div>
-                    <div className="cube-face  cube-face-left" style={{ borderColor: 'gold' }}>
-                      <img alt="" src={user.avatars[2].path} height="100%" />
-                    </div>
-                    <div className="cube-face  cube-face-right" style={{ borderColor: 'gold' }}>
-                      <img alt="" src={user.avatars[3].path} height="100%" />
-                    </div>
-                  </>
-                }
-              </div>}
+          {user.avatars && user.avatars.length > 0 &&
+            <div className="cube">
+              {user.avatars.length === 1 &&
+                <>
+                  <div className="cube-face  cube-face-front">
+                    <img alt="" src={user.avatars[0].path} height="100%" />
+                  </div>
+                  <div className="cube-face  cube-face-back">
+                    <img alt="" src={user.avatars[0].path} height="100%" />
+                  </div>
+                  <div className="cube-face  cube-face-left">
+                    <img alt="" src={user.avatars[0].path} height="100%" />
+                  </div>
+                  <div className="cube-face  cube-face-right">
+                    <img alt="" src={user.avatars[0].path} height="100%" />
+                  </div>
+                </>
+              }
+              {user.avatars.length === 2 &&
+                <>
+                  <div className="cube-face  cube-face-front">
+                    <img alt="" src={user.avatars[0].path} height="100%" />
+                  </div>
+                  <div className="cube-face  cube-face-back">
+                    <img alt="" src={user.avatars[1].path} height="100%" />
+                  </div>
+                  <div className="cube-face  cube-face-left">
+                    <img alt="" src={user.avatars[0].path} height="100%" />
+                  </div>
+                  <div className="cube-face  cube-face-right">
+                    <img alt="" src={user.avatars[1].path} height="100%" />
+                  </div>
+                </>
+              }
+              {user.avatars.length === 3 &&
+                <>
+                  <div className="cube-face  cube-face-front">
+                    <img alt="" src={user.avatars[0].path} height="100%" />
+                  </div>
+                  <div className="cube-face  cube-face-back">
+                    <img alt="" src={user.avatars[1].path} height="100%" />
+                  </div>
+                  <div className="cube-face  cube-face-left">
+                    <img alt="" src={user.avatars[2].path} height="100%" />
+                  </div>
+                  <div className="cube-face  cube-face-right">
+                    <img alt="" src={user.avatars[0].path} height="100%" />
+                  </div>
+                </>
+              }
+              {user.avatars.length >= 4 &&
+                <>
+                  <div className="cube-face  cube-face-front">
+                    <img alt="" src={user.avatars[0].path} height="100%" />
+                  </div>
+                  <div className="cube-face  cube-face-back">
+                    <img alt="" src={user.avatars[1].path} height="100%" />
+                  </div>
+                  <div className="cube-face  cube-face-left">
+                    <img alt="" src={user.avatars[2].path} height="100%" />
+                  </div>
+                  <div className="cube-face  cube-face-right">
+                    <img alt="" src={user.avatars[3].path} height="100%" />
+                  </div>
+                </>
+              }
+            </div>}
         </div>
       </div>
       <div className="cuna">
