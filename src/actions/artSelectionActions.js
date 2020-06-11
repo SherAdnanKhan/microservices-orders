4bbe054ef343b221204d9e5a80c8ec65fb8e3f71
Salwa M <codernist.s@gmail.com>
2020-06-11 15:08:07 +0500

    fixed art selection bug

--- src/actions/artSelectionActions.js
+++ src/actions/artSelectionActions.js
@@ -1,5 +1,6 @@
 import http from "../services/httpService"
 import { GET_ART, SELECT_USER } from "../constants/actionTypes";
+import { userKey } from "../constants/keys";
 
 export const getArt = () => dispatch => {
   http
@@ -21,6 +22,7 @@ export const newArt = (value, history) => dispatch => {
     http
       .post('/arts', value)
       .then(res => {
+        localStorage.setItem(userKey, JSON.stringify(res.data.data.user));
         history.push('/dashboard/start-favas');
       });
   }
@@ -30,6 +32,7 @@ export const selectArt = (value, history) => dispatch => {
   http
     .post('/arts/user-art-selection', value)
     .then(res => {
+      localStorage.setItem(userKey, JSON.stringify(res.data.data.user));
       let id = res?.data?.data?.user?.art?.id;
       let name = res?.data?.data?.user?.art?.name;
       localStorage.setItem('art_id', id);
