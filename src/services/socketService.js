77aabbd828c22bae4fe21f8d80e055e4220097e9
Salwa M <codernist.s@gmail.com>
2020-07-09 18:43:04 +0500

    created socket io singletone class to use one socket instance in allplaces

---
+++ src/services/socketService.js
@@ -0,0 +1,16 @@
+import io from 'socket.io-client';
+
+class Socket {
+  config = {
+    secure: true,
+    resource: process.env.REACT_APP_SOCKET_BASE_PATH,
+    path: process.env.REACT_APP_SOCKET_BASE_PATH,
+    transports: ['polling']
+  };
+
+  getSocketConection() {
+    return io.connect(process.env.REACT_APP_SOCKET_URL);
+  };
+};
+
+export default new Socket().getSocketConection()
