f1336a587c042d1b0e947dacced29423d84006d2
Salwa M <codernist.s@gmail.com>
2020-07-13 13:22:48 +0500

    fixed some design in studio

--- src/components/dashboard/layout/footer.js
+++ src/components/dashboard/layout/footer.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <div className="bottom">
-      <Link to="/dashboard/exhibition/new">
-        <i className="fas fa-plus" />
-      </Link>
     </div>
   );
 };
