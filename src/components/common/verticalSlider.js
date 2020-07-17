5fe8f5b4ed1bbd36c3cb2e96e0ca2f45ed8a0e63
Salwa M <codernist.s@gmail.com>
2020-07-17 12:40:04 +0500

    implemented carousel in start fav page

--- src/components/common/verticalSlider.js
+++ src/components/common/verticalSlider.js
@@ -3,7 +3,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
-const VerticalSlider = ({ children }) => {
+const VerticalSlider = ({ children, slidesToShow = 3, slidesToScroll = 3 }) => {
   const sliderRef = useRef();
 
   function PrevArrow({ onClick, style }) {
@@ -34,10 +34,36 @@ const VerticalSlider = ({ children }) => {
     infinite: false,
     speed: 500,
     vertical: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: slidesToShow,
+    slidesToScroll: slidesToScroll,
     nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: false,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 
   return (
