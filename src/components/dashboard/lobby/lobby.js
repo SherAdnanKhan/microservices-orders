766aafbd07bdaedd03b44ca0447237467a34fa6f
Salwa M <codernist.s@gmail.com>
2020-08-10 18:26:52 +0500

    warnings are removed

--- src/components/dashboard/lobby/lobby.js
+++ src/components/dashboard/lobby/lobby.js
@@ -268,6 +268,7 @@ const Lobby = () => {
           myGalleries={myGalleries}
           selectedGalleryId={galleryId}
           onGalleryId={getSelectedGalleryId}
+          Updatedpost={post}
         />
       }
       {showModalTurnOffCritque &&
