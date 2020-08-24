83e7d496a27f510a48958269beda62acf9d957e2
Salwa M <codernist.s@gmail.com>
2020-08-24 13:27:23 +0500

    created video chat desktop screen

--- src/components/video/groupVideo.js
+++ src/components/video/groupVideo.js
@@ -1,201 +1,91 @@
 
 import React from 'react';
 
-const GroupVideoCall=()=> {
+const GroupVideoCall = () => {
   return (
-      <React.Fragment>
-<div className="group-video ">
-    <div className="artist-group border-artist">
-      <div className="add-strq">
-        <div className="dosts dropdown">
-          <i className="fa fa-ellipsis-v" aria-hidden="true"></i>
-          <div className="dropdown-content">
-            <a href="/">Link 1</a>
-            <a href="/">Link 2</a>
-            <a href="/">Link 3</a>
+    <React.Fragment>
+      {/* <div className="group-video ">
+        <div className="artist-group border-artist">
+          <div className="head-main-user">
+            <div className="main-user">
+              <img className="add-user" src="/assets/images/avataricon.png" alt="add" />
+            </div>
           </div>
         </div>
-        <div className="video-classNameName">
-   
-        </div>
-        <div className="artist-name">
-          salwa
-        </div>
-      </div>
-      <div className="head-main-user">
-        <div className="main-user">
-          <img className="add-user"  src="/assets/images/avataricon.png" alt="add" />
-        </div>
-      </div>
-    </div>
 
-    <div className="artist-group border-artist">
-      <div className="add-strq">
-        <div className="dosts dropdown">
-          <i className="fa fa-ellipsis-v" aria-hidden="true"></i>
-          <div className="dropdown-content">
-            <a href="/">Link 1</a>
-            <a href="/">Link 2</a>
-            <a href="/">Link 3</a>
+        <div className="artist-group border-artist">
+          <div className="head-main-user">
+            <div className="main-user">
+              <img className="add-user" src="/assets/images/avataricon.png" alt="add" />
+            </div>
           </div>
         </div>
-        <div className="video-classNameName">
-   
-        </div>
-        <div className="artist-name">
-          salwa
-        </div>
-      </div>
-      <div className="head-main-user">
-        <div className="main-user">
-          <img className="add-user"  src="/assets/images/avataricon.png" alt="add" />
-        </div>
-      </div>
-    </div>
-    <div className="artist-group border-artist">
-      <div className="add-strq">
-        <div className="dosts dropdown">
-          <i className="fa fa-ellipsis-v" aria-hidden="true"></i>
-          <div className="dropdown-content">
-            <a href="/">Link 1</a>
-            <a href="/">Link 2</a>
-            <a href="/">Link 3</a>
+        <div className="artist-group border-artist">
+          <div className="head-main-user">
+            <div className="main-user">
+              <img className="add-user" src="/assets/images/avataricon.png" alt="add" />
+            </div>
           </div>
         </div>
-        <div className="video-classNameName">
-         
-        </div>
-        <div className="artist-name">
-          salwa
-        </div>
-      </div>
-      <div className="head-main-user">
-        <div className="main-user">
-          <img className="add-user"  src="/assets/images/avataricon.png" alt="add" />
-        </div>
-      </div>
-    </div>
-    <div className="artist-group border-artist">
-      <div className="add-strq">
-        <div className="dosts dropdown">
-          <i className="fa fa-ellipsis-v" aria-hidden="true"></i>
-          <div className="dropdown-content">
-            <a href="/">Link 1</a>
-            <a href="/">Link 2</a>
-            <a href="/">Link 3</a>
+        <div className="artist-group border-artist">
+          <div className="head-main-user">
+            <div className="main-user">
+              <img className="add-user" src="/assets/images/avataricon.png" alt="add" />
+            </div>
           </div>
         </div>
-        <div className="video-classNameName">
-
-        </div>
-        <div className="artist-name">
-          salwa
-        </div>
       </div>
-      <div className="head-main-user">
-        <div className="main-user">
-          <img className="add-user"  src="/assets/images/avataricon.png" alt="add" />
-        </div>
-      </div>
-    </div>
-
-
-  </div>
-  <div className="group-video bottom-video-section">
-    <div className="artist-group border-artist">
-      <div className="add-strq">
-        <div className="dosts dropdown">
-          <i className="fa fa-ellipsis-v" aria-hidden="true"></i>
-          <div className="dropdown-content">
-            <a href="/">Link 1</a>
-            <a href="/">Link 2</a>
-            <a href="/">Link 3</a>
+      <div className="group-video bottom-video-section">
+        <div className="artist-group border-artist">
+          <div className="head-main-user">
+            <div className="main-user">
+              <img className="add-user" src="/assets/images/avataricon.png" alt="add" />
+            </div>
           </div>
         </div>
-        <div className="video-classNameName">
 
-        </div>
-        <div className="artist-name">
-          salwa
-        </div>
-      </div>
-      <div className="head-main-user">
-        <div className="main-user">
-          <img className="add-user"  src="/assets/images/avataricon.png" alt="add" />
-        </div>
-      </div>
-    </div>
-
-    <div className="artist-group border-artist">
-      <div className="add-strq">
-        <div className="dosts dropdown">
-          <i className="fa fa-ellipsis-v" aria-hidden="true"></i>
-          <div className="dropdown-content">
-            <a href="/">Link 1</a>
-            <a href="/">Link 2</a>
-            <a href="/">Link 3</a>
+        <div className="artist-group border-artist">
+          <div className="head-main-user">
+            <div className="main-user">
+              <img className="add-user" src="/assets/images/avataricon.png" alt="add" />
+            </div>
           </div>
         </div>
-        <div className="video-classNameName">
-
-        </div>
-        <div className="artist-name">
-          salwa
-        </div>
-      </div>
-      <div className="head-main-user">
-        <div className="main-user">
-          <img className="add-user"  src="/assets/images/avataricon.png" alt="add" />
-        </div>
-      </div>
-    </div>
-    <div className="artist-group border-artist">
-      <div className="add-strq">
-        <div className="dosts dropdown">
-          <i className="fa fa-ellipsis-v" aria-hidden="true"></i>
-          <div className="dropdown-content">
-            <a href="/">Link 1</a>
-            <a href="/">Link 2</a>
-            <a href="/">Link 3</a>
+        <div className="artist-group border-artist">
+          <div className="head-main-user">
+            <div className="main-user">
+              <img className="add-user" src="/assets/images/avataricon.png" alt="add" />
+            </div>
           </div>
         </div>
-        <div className="video-classNameName">
-
-        </div>
-        <div className="artist-name">
-          salwa
-        </div>
-      </div>
-      <div className="head-main-user">
-        <div className="main-user">
-          <img className="add-user"  src="/assets/images/avataricon.png" alt="add" />
-        </div>
-      </div>
-    </div>
-    <div className="artist-group border-artist">
-      <div className="add-strq">
-        <div className="dosts dropdown">
-          <i className="fa fa-ellipsis-v" aria-hidden="true"></i>
-          <div className="dropdown-content">
-            <a href="/">Link 1</a>
-            <a href="/">Link 2</a>
-            <a href="/">Link 3</a>
+        <div className="artist-group border-artist">
+          <div className="head-main-user">
+            <div className="main-user">
+              <img className="add-user" src="/assets/images/avataricon.png" alt="add" />
+            </div>
           </div>
         </div>
-        <div className="video-classNameName">
-        </div>
-        <div className="artist-name">
-          salwa
+      </div> */}
+      {/* New Video Chat Starts Here */}
+      <div className="main">
+        <div className="own-Video"><video poster="/assets/images/avataricon.png" width="80" height="80px"></video></div>
+        <div className="settings-Icon"><i className="fas fa-ellipsis-h" /></div>
+        <div className="draw-Icon"><i className="fas fa-pen" /></div>
+        <div className="video-Icon"><i className="fas fa-video" /></div>
+        <div className="screen-Maximize"><i className="far fa-square" /></div>
+        <div className="item-List">
+          <div className="item border-item1"><video poster="/assets/images/avataricon.png" width="120" height="120px"></video></div>
+          <div className="item border-item2"><video poster="/assets/images/avataricon.png" width="120" height="120px"></video></div>
+          <div className="item border-item3"><video poster="/assets/images/avataricon.png" width="120" height="120px"></video></div>
+          <div className="item border-item4"><video poster="/assets/images/avataricon.png" width="120" height="120px"></video></div>
+          <div className="item border-item5"><video poster="/assets/images/avataricon.png" width="120" height="120px"></video></div>
+          <div className="item border-item1"><video poster="/assets/images/avataricon.png" width="120" height="120px"></video></div>
+          <div className="item border-item2"><video poster="/assets/images/avataricon.png" width="120" height="120px"></video></div>
+          <div className="item border-item4"><video poster="/assets/images/avataricon.png" width="120" height="120px"></video></div>
+          <div className="item border-item3"><video poster="/assets/images/avataricon.png" width="120" height="120px"></video></div>
         </div>
       </div>
-      <div className="head-main-user">
-        <div className="main-user">
-          <img className="add-user"  src="/assets/images/avataricon.png" alt="add" />
-        </div>
-      </div>
-    </div>
-  </div>
-  </React.Fragment>
+    </React.Fragment>
   );
 }
 export default GroupVideoCall;
