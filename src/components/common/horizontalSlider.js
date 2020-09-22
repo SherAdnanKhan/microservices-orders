3800aae68323dbc4ae633abc4eeb395ef3591826
Salwa M <codernist.s@gmail.com>
2020-09-22 15:04:38 +0500

    scroller added on horizontal slider

--- src/components/common/horizontalSlider.js
+++ src/components/common/horizontalSlider.js
@@ -7,36 +7,36 @@ import Slider from "react-slick";
 const HorizontalSlider = ({ children, slidesToShow = '' }) => {
   const sliderRef = useRef();
 
-  function PrevArrow({ onClick, style }) {
-    return (
-      <i
-        className="arrow-up fa fa-caret-up fa-3x"
-        onClick={onClick}
-        style={{ display: 'none' }}
-      >
-      </i>
-    );
-  }
-  function NextArrow({ onClick, style }) {
-    return (
-      <i
-        className="arrow-down fa fa-caret-down fa-3x"
-        onClick={onClick}
-        style={{ display: 'none' }}
-      >
-      </i>
-    );
-  }
+  // function PrevArrow({ onClick, style }) {
+  //   return (
+  //     <i
+  //       className="arrow-up fa fa-caret-up fa-3x"
+  //       onClick={onClick}
+  //       style={{ display: 'none' }}
+  //     >
+  //     </i>
+  //   );
+  // }
+  // function NextArrow({ onClick, style }) {
+  //   return (
+  //     <i
+  //       className="arrow-down fa fa-caret-down fa-3x"
+  //       onClick={onClick}
+  //       style={{ display: 'none' }}
+  //     >
+  //     </i>
+  //   );
+  // }
   const [settings] = useState({
-    arrows: true,
+    arrows: false,
     dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: slidesToShow ? slidesToShow : children?.length === 2 ? 2 : children?.length === 1 ? 1 : 3,
     slidesToScroll: 1,
 
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />
+    // nextArrow: <NextArrow />,
+    // prevArrow: <PrevArrow />
   });
 
   return (
