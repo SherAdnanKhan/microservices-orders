08f5e61d583a1a3dfdafe10b486731cfdfcd9747
Salwa M <codernist.s@gmail.com>
2020-10-26 17:54:58 +0500

    Fix design issue at lobby page and search record.

--- src/components/common/loader.js
+++ src/components/common/loader.js
@@ -5,8 +5,8 @@ const Loader = () => {
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
       style={{ margin: "auto", display: "block" }}
-      width="100px"
-      height="100px"
+      width="50px"
+      height="50px"
       viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
       <circle cx={50} cy={50} fill="none" stroke="#fff" strokeWidth={5} r={30} strokeDasharray="164.93361431346415 56.97787143782138">
         <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;360 50 50" keyTimes="0;1" />
