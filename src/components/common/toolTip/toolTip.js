49fb0bf04dbe8255deb113b301e206781e4b1aab
Salwa M <codernist.s@gmail.com>
2020-11-11 17:08:24 +0500

    feed created without image or video in mzflash group

--- src/components/common/toolTip/toolTip.js
+++ src/components/common/toolTip/toolTip.js
@@ -1,12 +1,12 @@
 import React from "react"
 import { useSelector } from "react-redux";
 import Tooltip from "react-tooltip";
-import { isMobile, convertHexToRGBA } from "../../../utils/helperFunctions";
+import { convertHexToRGBA } from "../../../utils/helperFunctions";
 
 const ToolTip = ({ position, id }) => {
   const { feelColor } = useSelector(state => state.feelColor);
+
   return (
-    !isMobile() &&
     <Tooltip
       backgroundColor={feelColor ? convertHexToRGBA(feelColor, 0.5) : convertHexToRGBA('#49C1D9', 0.5)}
       textColor="white"
