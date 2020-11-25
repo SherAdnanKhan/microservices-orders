6973e23ead5a077338b111b29beadfca9d5dfadb
Salwa M <codernist.s@gmail.com>
2020-11-25 15:01:39 +0500

    date of birth and agreement check and page added

---
+++ src/components/auth/agreement.js
@@ -0,0 +1,52 @@
+import React from "react";
+const Agreement = () => {
+  return (
+    <div className="agreement-page">
+      <p className="main-head">
+        ANY PARTICIPATION IN THIS SITE WILL CONSTITUTE ACCEPTANCE OF THIS AGREEMENT. IF YOU DONOT AGREE
+        TO ABIDE BY THE ABOVE, PLEASE DONOT USE THIS SITE.
+      </p>
+      <hr></hr>
+      <h4>Our Identity</h4>
+      <ul>Meuzm is
+        <li className="top">A Social / Professional Arts Society (So/ProArtsSo)(network)</li>
+        <li>An online platform</li>
+        <li>An arts search engine</li>
+        <li>An arts news and events notice provider</li>
+        <li>An online, site-specific money management system (MZCash, MZWallet)</li>
+        <li>A multi-layered system (a Super app)</li>
+        <li>Simple and intuitive</li>
+        <li>An outlet for expression</li>
+        <li>A place to gain</li>
+      </ul>
+      <ul>Meuzum in not
+        <li className="top">A one function plateform</li>
+        <li>solely visual</li>
+        <li>A travel company</li>
+        <li>A porn site</li>
+        <li>A text only site</li>
+        <li>A ride provider</li>
+        <li>Simple and intuitive</li>
+        <li>An all data search engine</li>
+      </ul>
+      <h4>AGREEING TO OUR TERMS</h4>
+      <p>At the outset of your journey, you will be asked to signup as a new Artist.When initially
+      making your Artist account, you will be first asked to read these TOS anda greet othem in
+      order to use our service.After agreeing, you will be asked to verify your age,
+      as well as other information.Meuzm requires that all personal information that you
+      enter be actual, real, true, and relevant to you, theArtist.</p>
+      <h4>Who can join</h4>
+      <p>All users must be atleast 13 years of age to join</p>
+      <h4>Termination of service</h4>
+      <p>Meuzm has the right to modify, suspend, or terminate and discontinue our service, initssolediscretion,at
+      anytime and without giving any prioror preemptive warning or notice.By agreeing to these terms,
+      you agree thatMeuzm is not and will not be liable to you for any modification, suspension, or termination
+      and /or discontinuance of the service and/or the data and information that you or your business has lost as a result.
+      In simple terms, this means that we have the right to completely shut our site down at a moments notice,
+        either temporarily or permanently, if need be, without any express notice before hand.</p>
+      <p> We also have the right to terminate,
+        suspend, or freeze any account with or without causeat anytime and without notice</p>
+    </div>
+  )
+}
+export default Agreement;
