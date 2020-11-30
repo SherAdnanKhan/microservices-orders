1f5b00c0eff705b039a93d55bba38c7d9dc704da
Salwa M <codernist.s@gmail.com>
2020-11-30 17:12:41 +0500

    pagination implemented on horizontal scroller

--- src/components/dashboard/chat/chatShortcut.js
+++ src/components/dashboard/chat/chatShortcut.js
@@ -6,9 +6,11 @@ import MeuzmLogo from "../../common/meuzmLogo";
 import UserContext from "../../../context/userContext";
 import ToolTip from "../../common/toolTip/toolTip";
 import socket from "../../../services/socketService";
+import HorizontalInfiniteScroller from "../../common/horizontalInfiniteScroller";
 
 const ChatShortCut = ({ onActiveConversation }) => {
   const [showShortcut, setShowShortcut] = useState(false);
+  const [currentShortcutPage, setCurrentShortcutPage] = useState(1);
   const { conversations, conversation } = useSelector(state => state.conversation)
   const currentUser = useContext(UserContext);
   let dispatch = useDispatch();
@@ -34,6 +36,12 @@ const ChatShortCut = ({ onActiveConversation }) => {
   const handleShowShortcut = () => {
     setShowShortcut(!showShortcut)
   }
+  const fetchNextConversations = () => {
+    console.log("next feeds")
+    dispatch(getAllConversations(currentShortcutPage + 1));
+    setCurrentShortcutPage(currentShortcutPage => currentShortcutPage + 1);
+  }
+
   return (
     <div className="chatUsers">
       <i
@@ -42,70 +50,76 @@ const ChatShortCut = ({ onActiveConversation }) => {
         data-tip="more"
         data-for="other chats" onClick={handleShowShortcut} ></i>
       <ToolTip id="more" position="top" />
+      <div className="shortcutOpen" id="chatUsers">
 
-      <div className="shortcutOpen">
         {showShortcut &&
-          <div className="chatShortcut">
-            {
-              conversations.data.length > 0 &&
-              conversations.data.map((conversation, index) => (
-                <div
-                  key={index}
-                  className="list-container"
-                  style={{ width: "auto" }}
-                  onClick={() => onActiveConversation(conversation)}
-                >
-                  {conversation.unread_messages_logs_count > 0 &&
-                    <div >
-                      {conversation.participants.length > 2
-                        ? (
-                          <span
-                            className='notify'
-                            style={{ borderColor: 'white' }}
-                          >
-                            {conversation.unread_messages_logs_count}
-                          </span>
-                        ) : (
-                          <span
-                            className='notify'
-                            style={{
-                              borderColor:
-                                conversation.participants[0].id !== currentUser.id
-                                  ? conversation.participants[0].feel.color_code
-                                  : conversation.participants[1].feel.color_code
-                            }}
-                          >
-                            {conversation.unread_messages_logs_count}
-                          </span>
-                        )
-                      }
-                    </div>
-                  }
-                  {conversation.participants.length > 0 &&
-                    <>
-                      {conversation.participants.length > 2
-                        ? (
-                          <MeuzmLogo />
-                        ) : (
-                          <>
-                            {conversation.participants[0].id !== currentUser.id &&
-                              <Avatar
-                                user={conversation.participants[0]}
-                              />
-                            }
-                            {conversation.participants[1].id !== currentUser.id &&
-                              <Avatar
-                                user={conversation.participants[1]}
-                              />
-                            }
-                          </>
-                        )
-                      }
-                    </>
-                  }
-                </div>
-              ))}
-          </div>
+          <HorizontalInfiniteScroller
+            dataLength={conversations?.data?.length}
+            onNextPage={fetchNextConversations}
+            hasMore={conversations?.next_page_url ? true : false}
+          >
+            <div className="chatShortcut" >
+              {
+                conversations.data.length > 0 &&
+                conversations.data.map((conversation, index) => (
+                  <div
+                    key={index}
+                    className="list-container"
+                    style={{ width: "auto" }}
+                    onClick={() => onActiveConversation(conversation)}
+                  >
+                    {conversation.unread_messages_logs_count > 0 &&
+                      <div >
+                        {conversation.participants.length > 2
+                          ? (
+                            <span
+                              className='notify'
+                              style={{ borderColor: 'white' }}
+                            >
+                              {conversation.unread_messages_logs_count}
+                            </span>
+                          ) : (
+                            <span
+                              className='notify'
+                              style={{
+                                borderColor:
+                                  conversation.participants[0].id !== currentUser.id
+                                    ? conversation.participants[0].feel.color_code
+                                    : conversation.participants[1].feel.color_code
+                              }}
+                            >
+                              {conversation.unread_messages_logs_count}
+                            </span>
+                          )
+                        }
+                      </div>
+                    }
+                    {conversation.participants.length > 0 &&
+                      <>
+                        {conversation.participants.length > 2
+                          ? (
+                            <MeuzmLogo />
+                          ) : (
+                            <>
+                              {conversation.participants[0].id !== currentUser.id &&
+                                <Avatar
+                                  user={conversation.participants[0]}
+                                />
+                              }
+                              {conversation.participants[1].id !== currentUser.id &&
+                                <Avatar
+                                  user={conversation.participants[1]}
+                                />
+                              }
+                            </>
+                          )
+                        }
+                      </>
+                    }
+                  </div>
+                ))}
+            </div>
+          </HorizontalInfiniteScroller>
         }
       </div>
     </div>
